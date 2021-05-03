36d9580a8e0f9c73c1f10416472072da3e72ca7e
alvaradoarturo <aalva058@ucr.edu>
2021-05-02 21:04:31 -0700

    CLI works for searching by document name

--- read_index.py
+++ read_index.py
@@ -14,9 +14,17 @@ def main():
         print('passed argument :: {}'.format(arg))
     print(args[0])
     print(args[1])
-    parsing_input(args[1])
+    if(args[0] == "--doc"):
+        print("doc")
+        parsing_input(args[1])
+    elif(args[0] == "--term"):
+        if(args[2] == "--doc"):
+            print("Term inside doc")
+        else:
+            print("Just term ")
+    else:
+        print("Error paring input")
 
 
 if __name__ == '__main__':
     main()
-
